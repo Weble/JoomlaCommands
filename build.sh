05d03d6fb8eebbda35ae81c041f88153da7a81b4
Skullbock <daniele@weble.it>
2020-04-03 16:51:27 +0200

    version 1.1.0

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
+# Remove old files
 rm -f build/packages/*.zip
 rm -f build/*.zip
 
+# Zip Plugin
 cd plugins/console/printconfig
-zip -q ../../../build/packages/plg_console_printconfig.zip ./*
+zip -qr ../../../build/packages/plg_console_printconfig.zip ./*
+
+# Zip Bin Files
 cd ../../../
-zip -q build/packages/bin_joomlacommands.zip bin/*
+zip -qr build/packages/bin_joomlacommands.zip bin/*
+
+# Zip Library
+cd libraries/joomla-commands
+zip -qr ../../build/packages/library_joomlacommands.zip vendor/* joomla-commands.xml
+cd ../../
 
 cd build
 
